c06633cc8f693bcc4d92f7b9d3922add90a35546
NodeWQS <95045783+NodeWQS@users.noreply.github.com>
2022-07-03 18:32:44 +0400

    Update system.module.js

--- system/system.module.js
+++ system/system.module.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 const controller = require('./system.controller')
 const middleware = require('./system.service')
 
-router.post('/', middleware ,controller.create)
+router.post('/' ,controller.create)
 router.get('/:id', controller.goto)
 
 module.exports = router
